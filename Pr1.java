6e64fcbf42eb9205f0446d26ae32e35c7996b9e4
Pankaj Ranher <pankajranher@gmail.com>
2026-08-04 14:56:24 +0530

    updated practice 2

--- Pr1.java
+++ Pr1.java
@@ -1,10 +1,10 @@
 class Pr1 {
     public static void main(String[] args) {
         System.out.println("Sunday");
-        System.out.println("Monday");
+        System.out.print("Monday");
         System.out.println("Tuesday");
         System.out.println("Wednesday");
-        System.out.println("Thursday");
+        System.out.print("Thursday");
         System.out.println("Friday");
         System.out.println("Saturday");
     }
